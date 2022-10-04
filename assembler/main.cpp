c1d98ba6ef3d94c8c478603eec77b15708166a2a
Abubakirov Vladimir <abubakirov04@mail.ru>
2022-10-05 01:49:38 +0300

    Delete unnecessary file

--- assembler/main.cpp
+++ assembler/main.cpp
@@ -1,11 +1,13 @@
 #include "assembler.h"
 
 int main(int argc, char *argv[]) {
+    int errorCode = 0;
+
     if (argc == 2) {
-        parseAsm((const char *) argv[1]);
+        errorCode = parseAsm((const char *) argv[1]);
     } 
     else if (argc == 3) {
-        parseAsm((const char *) argv[1], (const char *) argv[2]);
+        errorCode = parseAsm((const char *) argv[1], (const char *) argv[2]);
     }
     else {
         printf("Assembler takes 2 arguments");
