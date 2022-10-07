60ef8bca1faba1de18141ef4bcae749c6778b455
Abubakirov Vladimir <abubakirov04@mail.ru>
2022-10-08 02:02:02 +0300

    Added assembler with one argument

--- assembler/assembler.cpp
+++ assembler/assembler.cpp
@@ -48,8 +48,8 @@ int translateCommand(Strings *commands, const char *outputName){
         return INCORRECT_FILE_NAME;
     }
 
-    int *machineCommands = (int *) calloc(2 * commands->size, sizeof(int));
-    int commandCount = 0;
+    char *machineCommands = (char *) calloc(2 * commands->size, sizeof(int));
+    int commandCount = 0, machineCommandsSize = 0;
 
     for (int i = 0; i < commands->size; i++) {
         int languageLen = sizeof(language) / sizeof(language[0]);
@@ -57,21 +57,27 @@ int translateCommand(Strings *commands, const char *outputName){
         for (int j = 0; j < languageLen; j++) {
             char command[MAX_COMMAND_LENGTH] = {};
             int commandLength = 0;
+
             sscanf(commands->array[i], "%s %n", command, &commandLength);
 
             if (!strcasecmp(command, language[j])) {
                 if (j == PUSH) {
-                    int value = atoi(commands->array[i] + commandLength);
-                    *machineCommands = j;
-                    machineCommands++;
+                    int count = 0, commandId = j, arg1 = 0, arg2 = 0;
+                    parseLine(commands->array[i], &count, &commandId, &arg1, &arg2);
 
-                    *machineCommands = value;
+                    *machineCommands = commandId;
                     machineCommands++;
+                    machineCommandsSize++;
+
+                    *machineCommands = arg1;
+                    machineCommands += sizeof(int);
+                    machineCommandsSize += sizeof(int);
 
                     commandCount += 2;
                 } else {
                     *machineCommands = j;
                     machineCommands++;
+                    machineCommandsSize++;
 
                     commandCount++;
                 }
@@ -79,15 +85,104 @@ int translateCommand(Strings *commands, const char *outputName){
         }
     }
 
-    machineCommands -= commandCount;
+    machineCommands -= machineCommandsSize;
 
     fwrite(&SIGNATURE, sizeof(SIGNATURE), 1, output);
     fwrite(&VERSION, sizeof(VERSION), 1, output);
     fwrite(&commandCount, sizeof(commandCount), 1, output);
-    fwrite(machineCommands, sizeof(machineCommands[0]), commandCount, output);
+    fwrite(machineCommands, sizeof(machineCommands[0]), machineCommandsSize, output);
 
     fclose(output);
     free(machineCommands);
 
     return OK;
+}
+
+int parseLine(char *buf, int *count, int *commandId, int *arg1, int *arg2) {
+    if (!buf || !count || !commandId || !arg1 || !arg2) return NULL_PTR;
+
+    char command[MAX_COMMAND_LENGTH] = {};
+
+    // int val = sscanf(buf, "%s %3s %d", command, command, arg2);
+    // printf("%s %d %d", command, arg2, val);
+    // if (val == 2) {
+    //     if (sizeof(command) == 5 * 2) {         // utf-8
+    //         if (command[0] == 'r' && command[2] == 'x') {
+    //             *commandId = rMask + iMask + (*commandId);
+    //             *count = 2;
+    //             *arg1 = command[1] - 'a';
+
+    //             return OK;
+    //         }
+
+    //         return INCORRECT_FORMAT;
+    //     } else {
+    //         return INCORRECT_FORMAT;
+    //     }
+    // }
+
+    // val = sscanf(buf, "%s [%s %d]", command, command, arg2);
+    // printf("%d", val);
+    // if (val == 2) {
+    //     printf("IN");
+    //     if (sizeof(command) == 5 * 2) {         // utf-8
+    //         if (command[0] == 'r' && command[2] == 'x') {
+    //             *commandId = mMask + rMask + iMask + (*commandId);
+    //             *count = 2;
+    //             *arg1 = command[1] - 'a';
+
+    //             return OK;
+    //         }
+
+    //         return INCORRECT_FORMAT;
+    //     } else {
+    //         return INCORRECT_FORMAT;
+    //     }
+    // }
+
+    val = sscanf(buf, "%s %d", command, arg1);
+    if (val == 2) {
+        *commandId = iMask + (*commandId);
+        *count = 1;
+        return OK;
+    }
+
+    val = sscanf(buf, "%s %s", command, command);
+    if (val == 2) {
+        if (sizeof(command) == 5 * 2) {         // utf-8
+            if (command[0] == 'r' && command[2] == 'x') {
+                *commandId = rMask + (*commandId);
+                *count = 1;
+                *arg1 = command[1] - 'a';
+            }
+        } else {
+            return INCORRECT_FORMAT;
+        }
+    }
+
+    val = sscanf(buf, "%s [%d]", command, arg1);
+    if (val == 2) {
+        *commandId = mMask + iMask + (*commandId);
+        *count = 1;
+        return OK;
+    }
+
+    val = sscanf(buf, "%s [%s]", command, command);
+    if (val == 2) {
+        if (sizeof(command) == 5 * 2) {         // utf-8
+            if (command[0] == 'r' && command[2] == 'x') {
+                *commandId = mMask + rMask + (*commandId);
+                *count = 1;
+                *arg1 = command[1] - 'a';
+
+                return OK;
+            }
+
+            return INCORRECT_FORMAT;
+        } else {
+            return INCORRECT_FORMAT;
+        }
+    }
+
+    return INCORRECT_FORMAT;
 }
--- assembler/assembler.h
+++ assembler/assembler.h
@@ -11,12 +11,17 @@
 const int VERSION = 1;
 const int SIGNATURE = 74 * 256 + 67;  // CJ
 
+const char iMask = 0x10;
+const char rMask = 0x20;
+const char mMask = 0x40;
+
 enum AsmErrorCodes {
     OK                      =  0,
     INCORRECT_FILE_NAME     = -1,
     CANNOT_OPEN_FILE        = -2,
     COMMANDS_NULL           = -3,
     NULL_PTR                = -4,
+    INCORRECT_FORMAT        = -5,
 };
 
 const int MAX_COMMAND_LENGTH = 10;
@@ -28,5 +33,7 @@ int checkCommands(Strings *commands);
 int parseAsm(const char *fileName, const char *outputName = baseOutputName);
 
 int translateCommand(Strings *commands, const char *outputName);
+
+int parseLine(char *buf, int *count, int *commandId, int *arg1, int *arg2);
 
 #endif
